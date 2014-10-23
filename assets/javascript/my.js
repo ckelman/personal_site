0bda702200d3d8e8813e2c6c8989549589a68856
ckelman <ckelman@u.rochester.edu>
2014-10-23 09:20:00 -0400

    stopped using angularJS framework.  It wan't necessary for this site and was causing issues with scrolling links.

--- assets/javascript/my.js
+++ assets/javascript/my.js
@@ -1,9 +1,85 @@
+
+
+
+
 $(document).ready(function () {
+
+  $("#nine-board-panel").hide();
+    $("#java-game-mac-panel").hide();
+    $("#java-game-pc-panel").hide();
+    $("#football-panel").show();
+
+  $("#football-toggle").click(function(){
+    $("#nine-board-panel").hide();
+    $("#java-game-mac-panel").hide();
+    $("#java-game-pc-panel").hide();
+    $("#football-panel").show();
+
+    $("#football-pill").removeClass("active");
+    $("#nine-board-pill").removeClass("active");
+    $("#java-game-pc-pill").removeClass("active");
+    $("#java-game-mac-pill").removeClass("active");
+
+    $("#football-pill").addClass("active");
+
+  });
+
+  $("#nine-board-toggle").click(function(){  
+    $("#java-game-mac-panel").hide();
+    $("#java-game-pc-panel").hide();
+    $("#football-panel").hide();
+
+    $("#nine-board-panel").show();
+
+    $("#football-pill").removeClass("active");
+    $("#nine-board-pill").removeClass("active");
+    $("#java-game-pc-pill").removeClass("active");
+    $("#java-game-mac-pill").removeClass("active");
+
+    $("#nine-board-pill").addClass("active");
+
+  });
+
+  $("#java-game-pc-toggle").click(function(){
+    $("#football-panel").hide();
+    $("#nine-board-panel").hide();
+    $("#java-game-mac-panel").hide();
+    $("#java-game-pc-panel").show();
+
+    $("#football-pill").removeClass("active");
+    $("#nine-board-pill").removeClass("active");
+    $("#java-game-pc-pill").removeClass("active");
+    $("#java-game-mac-pill").removeClass("active");
+
+    $("#java-game-pc-pill").addClass("active");
+  });
+
+  $("#java-game-mac-toggle").click(function(){
+    $("#football-panel").hide();
+    $("#nine-board-panel").hide();
+    $("#java-game-pc-panel").hide();
+    $("#java-game-mac-panel").show();
+
+    $("#football-pill").removeClass("active");
+    $("#nine-board-pill").removeClass("active");
+    $("#java-game-pc-pill").removeClass("active");
+    $("#java-game-mac-pill").removeClass("active");
+
+    $("#java-game-mac-pill").addClass("active");
+  });
+
 
 
     $(window).on('resize', function(){
 
+
+
       var $containerWidth = $(window).width();
+      var $containerHeight = $(window).height();
+
+
+
+
 
       if ($containerWidth <= 770) {
         $('#landing').css('height', '1000px');
@@ -34,6 +110,19 @@ $(document).ready(function () {
 
       }
 
-    });
+       if($containerHeight <= 600){
+        $('#landing').css("height","600");
+        $('#landing').css("background-size","100% 600");
 
+      }
+      else{
+        $('#landing').css("height","100%");
+        $('#landing').css("background-size","100% 100%");
+      }
+
+
+    });
 });
+
+
+
